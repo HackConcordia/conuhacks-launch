bb992999948aa8ba00d2ae0854fcd55bfc09e825
MHUZAIFA <mhuzaifa.career@outlook.com>
2025-10-29 17:45:02 -0400

    build fix

--- app/data/header.data.ts
+++ app/data/header.data.ts
@@ -1,15 +1,6 @@
 import { IconType } from 'react-icons';
 import { FaFacebookF, FaInstagram, FaLinkedinIn, FaRegEnvelope } from 'react-icons/fa';
 import { FaX } from 'react-icons/fa6';
-import { TranslationObject } from './translationObject.data';
-
-export const getNavLinks = (t: TranslationObject): { href: string; label: string }[] => [
-    { href: '#home', label: t.Header.home },
-    { href: '#conuhacks', label: t.Header.conuhacks },
-    // { href: '#events', label: t.Header.events },
-    { href: '#team', label: t.Header.team },
-    { href: '#faq', label: t.Header.faq },
-];
 
 export const pageToSectionMap: Record<string, string> = {
     '/legacy-teams': 'team',
